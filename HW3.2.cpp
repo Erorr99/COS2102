14e2d9cc0ace2915204d21a265403a8f74eda0cf
MILLIONx <blueboych6@gmail.com>
2023-07-25 20:26:24 +0700

    2

---
+++ HW3.2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+#include <iostream>
+
+using namespace std;
+
+class Stack
+{
+private:
+    int *array;
+    int size;
+
+public:
+    // consturctor
+
+    Stack() {}
+    Stack() {}
+
+    // desturctor
+
+    ~Stack() {}
+    // set
+    void setArray() {}
+    void setSize() {}
+    void push() {}
+    void pop() {}
+    void isFull() {}
+    void isEmpty() {}
+    // get
+
+    int get() {}
+    int count() {}
+
+    // show
+    void show() {}
+};
+
+int main()
+{
+    return 0;
+};
