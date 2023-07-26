962f979c3302a6d3cc47d17579f91680db9aa3a5
MILLIONx <blueboych6@gmail.com>
2023-07-26 21:52:12 +0700

    2

--- HW3.cpp
+++ HW3.cpp
@@ -63,6 +63,15 @@ public:
         rearea = width * length;
         return rearea;
     }
+
+    // show
+    void show()
+    {
+        cout << "width = " << width << endl;
+        cout << "length = " << length << endl;
+        cout << "perimeter = " << perimeter() << endl;
+        cout << "area = " << area() << endl;
+    }
 };
 
 class MyRect
@@ -70,7 +79,6 @@ class MyRect
 private:
     int row, col;
     Rectangle **array;
-
     void set(int row, int col)
     {
         this->row = row;
@@ -79,8 +87,17 @@ private:
         this->array = new Rectangle *[row];
         for (int i = 0; i < row; i++)
         {
-            array[i] = new Rectangle[col];
+            this->array[i] = new Rectangle[col];
         };
+    }
+
+    void del()
+    {
+        for (int i = 0; i < row; i++)
+        {
+            delete[] array[i];
+        };
+        delete[] array;
     }
 
 public:
@@ -93,20 +110,13 @@ public:
     }
     MyRect(int row, int col)
     {
-        set(this->row, this->col);
+        set(row, col);
     }
 
     // destuctor
     ~MyRect()
     {
-        if (array != 0)
-        {
-            for (int i = 0; i < row; i++)
-            {
-                delete[] array[i];
-            };
-            delete[] array;
-        };
+        this->del();
     }
 
     // set
@@ -118,41 +128,62 @@ public:
         {
             for (int j = 0; j < col; j++)
             {
-                cout << "width = " << endl;
+                cout << "width = ";
                 cin >> w;
-                cout << "length = " << endl;
+                cout << "length = ";
                 cin >> l;
                 Rectangle r(w, l);
-                this->array[i][j] = r;
+                // this->array[i][j] = r;
+                this->array[i][j].setLength(l);
+                this->array[i][j].setWidth(w);
             }
         }
     }
 
-    void Re(Rectangle array)
+    void setwl(int row, int col, double width, double length)
     {
+        array[row][col].setWidth(width);
+        array[row][col].setLength(length);
     }
-    void reset(int row, int col)
+
+    // set row and col
+
+    void setRow(int row)
     {
-        if (row == row && col == col)
+        this->row = row;
+    }
+    void setCol(int col)
+    {
+        this->col = col;
+    }
+
+    void object(int row, int col, Rectangle &a)
+    {
+        this->array[row][col] = a;
+    }
+
+    void reset(int newrow, int newcol)
+    {
+        if (newrow == row && newcol == col)
         {
             return;
         }
-        MyRect();
-        set(row, col);
+        this->del();
+        set(newrow, newcol);
     }
 
     Rectangle getRect(int row, int col)
     {
         return array[row][col];
-        ;
     }
     void show()
     {
+        cout << "row [" << this->row << "][" << this->col << "]" << endl;
         for (int i = 0; i < row; i++)
         {
             for (int j = 0; j < col; j++)
             {
-                cout << "Rectangle wid= " << array[i] << "hight =" << array[j] << endl;
+                this->array[i][j].show();
             }
         }
     }
@@ -160,9 +191,66 @@ public:
 
 int main()
 {
-    MyRect p(2, 3);
-    p.setRect();
-    p.show();
+    int row, col;
+    double width, length;
+    MyRect n;
 
+    cout << "========== set 1 ==========" << endl;
+    MyRect mr(2, 3);
+    mr.setRect();
+    mr.show();
+
+    cout << "========== set 2 ==========" << endl;
+    do
+    {
+        cout << "row = ";
+        cin >> row;
+    } while (row > 2);
+    do
+    {
+        cout << "col = ";
+        cin >> col;
+    } while (col > 3);
+    Rectangle r1(1, 2);
+    mr.object(row, col, r1);
+    mr.show();
+
+    cout << "========== set 3 ==========" << endl;
+    do
+    {
+        cout << "row = ";
+        cin >> row;
+    } while (row > 1);
+    do
+    {
+        cout << "col = ";
+        cin >> col;
+    } while (col > 2);
+
+    cout << "width = ";
+    cin >> width;
+    cout << "length = ";
+    cin >> length;
+    mr.setwl(row, col, width, length);
+
+    cout << "========== get ==========" << endl;
+    do
+    {
+        cout << "row = ";
+        cin >> row;
+    } while (row > 1);
+    do
+    {
+        cout << "col = ";
+        cin >> col;
+    } while (col > 2);
+    Rectangle r2 = mr.getRect(row, col);
+    r2.show();
+
+    cout << "========== set before Reset ==========" << endl;
+    mr.show();
+    cout << "========== set after Reset ==========" << endl;
+    mr.reset(3, 4);
+    mr.show();
     return 0;
 };
