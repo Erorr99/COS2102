5c2f6c5814681380208f32bfc561faac98c3d591
MILLIONx <blueboych6@gmail.com>
2023-08-03 11:21:30 +0700

    d

--- HW2.cpp
+++ HW2.cpp
@@ -8,7 +8,6 @@ class Point
     static int num;
     double x, y;
     string name1;
-    char name2[20];
 
 public:
     // constructor
@@ -18,7 +17,6 @@ public:
         this->x = 0.0;
         this->y = 0.0;
         this->name1 = "";
-        strcpy(this->name2, "");
     }
     Point(double x)
     {
@@ -26,7 +24,6 @@ public:
         this->x = x;
         this->y = 0.0;
         this->name1 = "";
-        strcpy(this->name2, "");
     };
     Point(double x, double y)
     {
@@ -34,7 +31,6 @@ public:
         this->x = x;
         this->y = y;
         this->name1 = "";
-        strcpy(this->name2, "");
     };
     Point(double x, double y, string name1)
     {
@@ -42,26 +38,15 @@ public:
         this->x = x;
         this->y = y;
         this->name1 = "name1";
-        strcpy(this->name2, "");
     };
-    Point(double x, double y, string name1, char *name2)
-    {
-        num++;
-        this->x = x;
-        this->y = y;
-        this->name1 = name1;
-        strcpy(this->name2, "name2");
-    }
 
     // get
     double getX() { return this->x; };
     double getY() { return this->y; };
     string getName1() { return this->name1; };
-    char *getName2() { return this->name2; }
 
     // set
     void setName1(string name1) { this->name1 = name1; };
-    void setName2(char *name2) { strcpy(this->name2, name2); };
     void setXY(double x, double y)
     {
         this->x = x;
@@ -82,7 +67,18 @@ public:
         cout << "x = " << this->x << endl;
         cout << "y = " << this->y << endl;
         cout << "name1 = " << this->name1 << endl;
-        cout << "name2 = " << this->name2 << endl;
+    }
+
+    double dot(Point &p)
+    {
+        p.x += 10.5; // ลองบวกค่า
+        p.y += 7.8;  // ลองบวกค่า
+        return this->x * p.x + this->y * p.y;
+    };
+
+    Point midpoint(Point &p1, Point &p2)
+    {
+        return Point((p1.x + p2.x) / 2, (p1.y + p2.y) / 2, "midpoint");
     }
 };
 
@@ -91,33 +87,35 @@ int Point::num;
 int main()
 {
     cout << "count " << Point::count() << endl;
-    Point p1;
-    p1.show();
-    cout << "=================================" << endl;
-    cout << "count " << Point::count() << endl;
-    {
-        Point p2(2.0, 4.2);
-        p2.setName1("B_ONE");
-        p2.setName2("B_TWO");
-        cout << "X : " << p2.getX() << endl;
-        cout << "Y : " << p2.getY() << endl;
-        cout << "name 1 :" << p2.getName1() << endl;
-        cout << "name 1" << p2.getName2() << endl;
-        cout << "=================================" << endl;
-        Point p3(4.2, 6.4);
-        p3.setName1("B_One");
-        p3.show();
-        cout << "count " << Point::count() << endl;
-    };
-    cout << "count " << Point::count() << endl;
-    cout << "=================================" << endl;
-    Point p4(3.0, 4.0);
-    p4.show();
-    cout << "=================================" << endl;
-    Point p5(6.5);
-    p5.show();
-    cout << "=================================" << endl;
-    cout << "count " << Point::count() << endl;
+    Point p1(2, 3, "s");
+    Point p2(4, 5, "s1");
+    cout << "s.dot(s1)" << p1.dot(p2) << endl;
+    Point mid = p1.midpoint(p1, p2);
+    mid.setName1("p1,p2,midpoi");
+    mid.show();
+
+    // cout << "=================================" << endl;
+    // cout << "count " << Point::count() << endl;
+    // {
+    //     Point p2(2.0, 4.2);
+    //     p2.setName1("B_ONE");
+    //     cout << "Y : " << p2.getY() << endl;
+    //     cout << "name 1 :" << p2.getName1() << endl;
+    //     cout << "=================================" << endl;
+    //     Point p3(4.2, 6.4);
+    //     p3.setName1("B_One");
+    //     p3.show();
+    //     cout << "count " << Point::count() << endl;
+    // };
+    // cout << "count " << Point::count() << endl;
+    // cout << "=================================" << endl;
+    // Point p4(3.0, 4.0);
+    // p4.show();
+    // cout << "=================================" << endl;
+    // Point p5(6.5);
+    // p5.show();
+    // cout << "=================================" << endl;
+    // cout << "count " << Point::count() << endl;
 
     return 0;
 }
