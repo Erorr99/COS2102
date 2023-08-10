03d67c915a0864ca4ff17007a4983f3df68d21eb
MILLIONx <blueboych6@gmail.com>
2023-08-10 10:09:12 +0700

    .

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,2 +1,5 @@
-
-    {
+        // double midX = (d1.x + d2.x) / 2.0;
+        // double midY = (d1.y + d2.y) / 2.0;
+        // string name = "midpoint(" + d1.name1 + "," + d2.name1 + ")";
+        // Point midPoint(midX, midY, name);
+        // return midPoint;
